a6de2fbb264569a0492679ff8c50e61e8a787067
moskalev27 <moskalev27@mail.ru>
2020-11-17 17:54:35 +0000

    Простой сервер

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-require('http')
-  .Server((req, res) => {
-    if (req.url === '/result4/') {
-      const CORS = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-        'Access-Control-Allow-Headers': 'x-test, Content-Type, Access-Control-Allow-Headers'
-      };
-
-      const result = {
-        message: 'moskalev27',
-        'x-result': req.headers['x-test']
-      };
-      let body = '';
-      req.on('data', ch => (body += ch))
-        .on('end', () => {
-          result['x-body'] = body;
-          res.writeHead(200, { ...CORS, 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(result));
+const CORS = {
+  'Access-Control-Allow-Origin': 'https://kodaktor.ru',
+  'Access-Control-Allow-Methods': 'GET,PUT,DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+const s = require('http').Server((req, res) => {
+    if (req.url === '/ru') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8', ...CORS });
+        res.write('<h1><i>Да</i></h1>\n'); 
+    } else if (req.url === '/doc/new/vasya-new.htm') {
+        res.write('<h1><i>Vasya Web Page</i></h1>\n');
+    } else if (req.url === '/download') {
+        res.writeHead(200, { 
+         'Content-Disposition': 'attachment; filename="file.txt"'
         });
+        res.write('Файл\n');        
+    } else {
+        res.write('OK\n');
     }
-  })
-  .listen(4321, () => console.log('Server start OK')); 
+    res.end();
+});
+s.listen(4321); 
